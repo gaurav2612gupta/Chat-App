ad1cf0e57cadcc79f7db5c9540ae20869e56876a
Gaurav Gupta <gaurav1122gupta@gmail.com>
2022-06-29 01:15:38 +0530

    some server changes

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+let socket = io();
+
+socket.on('connect', () => {
+    console.log("Connected to server");
+
+})
+socket.on('disconnect', () => {
+    console.log("Disonnected to server");
+})
+
+socket.on('newMessage', (message) => {
+    console.log("newMessage",message);
+})
--- server/server.js
+++ server/server.js
@@ -15,6 +15,29 @@ app.use(express.static(publicPath))
 
 io.on('connection',(socket)=>{
     console.log("A new User just Connected");
+
+    socket.emit('newMessage',{
+        name:"Admin",
+        text: "Hi Welcome to Chat Room",
+        cretedAt : new Date().getTime()
+    })
+    
+    socket.broadcast.emit('newMessage',{
+        name:"Admin",
+        text: "New User Joined",
+        cretedAt : new Date().getTime()
+
+    })
+
+    socket.on('createMessage',(message)=>{
+        console.log("createMessage", message);
+        io.emit('newMessage',{
+            from : message.from,
+            text : message.text,
+            cretedAt : new Date().getTime()
+        })
+    })
+
     socket.on('disconnect',()=>{
         console.log("User was disonnected");
     })
